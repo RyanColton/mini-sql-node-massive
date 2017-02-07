859eb7fc13a49cb345869516b11bfbcb1567ce2f
Ryan Colton <ryancolton@Ryans-MacBook-Pro.local>
2017-02-07 10:08:17 -0700

    Finished Project

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-
+var massive = require('massive');
+var connectionString = 'postgres://postgres:Ryan936169@localhost/sandbox';
+var massiveInstance = massive.connectSync({connectionString: connectionString});
 var app = module.exports = express();
+var controller = require('./controller.js')
 app.use(bodyParser.json());
 app.use(cors());
+app.set('db', massiveInstance);
+// var db = app.get('db');
 
-app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
+// db.new_plane(function(err, planes){
+//   console.log(err, 'plane added')
+// })
+// db.get_planes(function(err, planes){
+//   console.log(err, planes)
+// })
+
+controller.getPlanes();
+
+app.listen('1138', function(){
+  console.log("Successfully listening on : 1138")
 })
-
